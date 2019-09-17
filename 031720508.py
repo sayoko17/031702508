3a5a623cb815034faf490a5fd436f82f3763cda4
sayoko17 <54976956+sayoko17@users.noreply.github.com>
2019-09-17 10:10:11 +0800

    函数

--- 031720508.py
+++ 031720508.py
@@ -9,15 +9,18 @@ city1= ''
 district = ''
 zhixia=['北京','天津','上海','重庆']
 dic={}
-line = input()
-line0 = line.split("!")
-if(line0[0]=='1'):
-    line1 = line0[1].split(",")
+def namenum(lista):
+    line1 = lista[1].split(",")
     add = re.sub(r'1\d{10}','',line1[1])
     tel = re.findall(r'1\d{10}',line1[1])
     dic['姓名']=line1[0]
     dic['手机']=tel[0]
-    PATTERN = r'([\u4e00-\u9fa5]{2,5}?(?:省|自治区|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:村|镇|街道|乡)){0,1}'
+    return add
+line = input()
+line0 = line.split("!")
+if(line0[0]=='1'):
+    add = namenum(line0)
+    PATTERN = r'([\u4e00-\u9fa5]{2,5}?(?:省|自治区|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市|盟|自治州)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市|旗)){0,1}([\u4e00-\u9fa5]{2,7}?(?:村|镇|街道|乡)){0,1}'
     pattern = re.compile(PATTERN)
     m = pattern.search(add)
 #如果是直辖
@@ -60,12 +63,8 @@ if(line0[0]=='1'):
 
         finaladd=[province,city,city1,district,last1[0]]
 if(line0[0]=='2'):
-    line1 = line0[1].split(",")
-    add = re.sub(r'1\d{10}','',line1[1])
-    tel = re.findall(r'1\d{10}',line1[1])
-    dic['姓名']=line1[0]
-    dic['手机']=tel[0]
-    PATTERN = r'([\u4e00-\u9fa5]{2,5}?(?:省|自治区|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:村|镇|街道|乡)){0,1}([\u4e00-\u9fa5]{2,7}?(?:路|大街|巷)){0,1}([0-9]{1,5}?(?:号)){0,1}'
+    add = namenum(line0)
+    PATTERN = r'([\u4e00-\u9fa5]{2,5}?(?:省|自治区|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市|盟|自治州)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市|旗)){0,1}([\u4e00-\u9fa5]{2,7}?(?:村|镇|街道|乡)){0,1}([\u4e00-\u9fa5]{2,7}?(?:路|大街|巷)){0,1}([0-9]{1,5}?(?:号)){0,1}'
     pattern = re.compile(PATTERN)
     m = pattern.search(add)
     if (add[0:2] in zhixia):
@@ -119,12 +118,8 @@ if(line0[0]=='2'):
 
         finaladd=[province,city,city1,district,daolu,hao,last1[0]]
 if(line0[0]=='3'):
-    line1 = line0[1].split(",")
-    add = re.sub(r'1\d{10}','',line1[1])
-    tel = re.findall(r'1\d{10}',line1[1])
-    dic['姓名']=line1[0]
-    dic['手机']=tel[0]
-    PATTERN = r'([\u4e00-\u9fa5]{2,5}?(?:省|自治区|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:村|镇|街道|乡)){0,1}([\u4e00-\u9fa5]{2,7}?(?:路|大街|巷)){0,1}([0-9]{1,5}?(?:号)){0,1}'
+    add = namenum(line0)
+    PATTERN = r'([\u4e00-\u9fa5]{2,5}?(?:省|自治区|市)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市|盟|自治州)){0,1}([\u4e00-\u9fa5]{2,7}?(?:区|县|州|市|旗)){0,1}([\u4e00-\u9fa5]{2,7}?(?:村|镇|街道|乡)){0,1}([\u4e00-\u9fa5]{2,7}?(?:路|大街|巷)){0,1}([0-9]{1,5}?(?:号)){0,1}'
     pattern = re.compile(PATTERN)
     m = pattern.search(add)
     if (add[0:2] in zhixia):
@@ -180,7 +175,6 @@ if(line0[0]=='3'):
 dic['地址']=finaladd
 json = json.dumps(dic,ensure_ascii=False,indent=4)
 print (json)
-
 #print("电话：",tel)
 #print("地址：",add)
     
